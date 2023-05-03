4033a5e9c5f7847156a6dedfa566970510945be8
m1lky4 <temanafanin1337@gmail.com>
2023-05-03 13:45:51 +0200

    finished color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
 
+let randomColor;
+
+start.addEventListener('click', () => {
+   randomColor = setInterval(() => {
+body.style.backgroundColor = getRandomHexColor();
+    },1000);
+    start.setAttribute('disabled', '');
+    stop.removeAttribute('disabled');
+});
+
+
+stop.addEventListener('click', () => {
+    clearInterval(randomColor);
+    stop.setAttribute('disabled', '');
+    start.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
